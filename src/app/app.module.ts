32a284e475f5fa51a0fb6ea18e2bbdf060cf0987
ayman elshiekh <a.elshiekh@najmtech.com>
2022-03-08 14:07:21 +0200

    Application Worked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { FormCategoryComponent } from './form-category/form-category.component';
+import { ProductComponent } from './product/product.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './home/home.component';
+import { FormProductComponent } from './form-product/form-product.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    FormCategoryComponent,
+    ProductComponent,
+
+    HomeComponent,
+
+    FormProductComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
